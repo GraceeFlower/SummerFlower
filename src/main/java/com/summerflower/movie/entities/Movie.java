1ced01f7b959f8a2a4021cc568b17100344f307c
GraceeFlower <1491644103@qq.com>
2020-04-13 12:22:10 +0800

    [WM] feat: new movie entity

--- src/main/java/com/summerflower/movie/entities/Movie.java
+++ src/main/java/com/summerflower/movie/entities/Movie.java
@@ -1,7 +1,119 @@
 package com.summerflower.movie.entities;
 
-public class Movie {
-    
-    private int id;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
+import org.springframework.data.relational.core.mapping.Table;
 
+@Table("movies")
+public class Movie {
+
+    @Id
+    private int id;
+    private String url;
+    private int year;
+    private String title;
+    private float rating;
+    @Column("original_title")
+    private String originalTitle;
+    private String directors;
+    private String casts;
+    private String genres;
+    @Column("image")
+    private String imageUrl;
+
+    public Movie(int id, String url, int year, String title, float rating, String originalTitle, String directors, String casts, String genres, String imageUrl) {
+        this.id = id;
+        this.url = url;
+        this.year = year;
+        this.title = title;
+        this.rating = rating;
+        this.originalTitle = originalTitle;
+        this.directors = directors;
+        this.casts = casts;
+        this.genres = genres;
+        this.imageUrl = imageUrl;
+    }
+
+    public Movie() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getDirectors() {
+        return directors;
+    }
+
+    public void setDirectors(String directors) {
+        this.directors = directors;
+    }
+
+    public String getCasts() {
+        return casts;
+    }
+
+    public void setCasts(String casts) {
+        this.casts = casts;
+    }
+
+    public String getGenres() {
+        return genres;
+    }
+
+    public void setGenres(String genres) {
+        this.genres = genres;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
